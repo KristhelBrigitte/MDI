ac6354281caf68f81e6dc36dcec0d310275fb17f
Brigitte <kristhelbri@gmail.com>
2021-11-04 23:49:40 -0600

    commit actualizacion form entradas

--- Practica ClasesMDI/frmAsignar.cs
+++ Practica ClasesMDI/frmAsignar.cs
@@ -21,32 +21,6 @@ namespace Practica_ClasesMDI
             InitializeComponent();
         }
 
-      /*
-            int cantSeleccion=0;
-            Button tipoEntrada = (Button) sender;
-            Cliente cliente = global.buscarCliente(txtCedula.Text);
-       
-            if (VerificarSelecccion() == null)
-            {
-                MessageBox.Show("Error, debe seleccionar la cantidad de enrtadas");
-            }
-            else if (cliente != null)
-            {
-                int cant = cliente.cantidadEntradas();
-                if (cant < 3 && (cant+cantSeleccion)<3)
-                {
-                    cliente.asignarEntrada(cantSeleccion,)
-                }
-                else
-                {
-                    MessageBox.Show("El cliente no puede tener más de 3 entradas!");
-                }
-            }
-            else
-            {
-                MessageBox.Show("El cliente no está registrado");
-            }    
-       */
 
         public RadioButton VerificarSelecccion()
         {
@@ -65,32 +39,39 @@ namespace Practica_ClasesMDI
         private void btnGeneral_Click(object sender, EventArgs e)
         {
             //ArrayList gramillas=global.getGramilla();
-            int cant = 0;  
-            if (VerificarSelecccion() == null)
+            if (global.getGramilla().Count == 0)
             {
-                MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                MessageBox.Show("No quedan entradas!");
             }
             else
             {
-                int cantSeleccion = int.Parse(VerificarSelecccion().Text);
-                Cliente cliente = global.buscarCliente(txtCedula.Text);
-                if (cliente != null)
+                int cant = 0;  
+                if (VerificarSelecccion() == null)
                 {
-                    cant = cliente.cantidadEntradas();
-                    if (cant < 3 && (cant + cantSeleccion) < 4)
-                    { 
-                        cliente.asignarEntrada(cantSeleccion,global.primerGR());
-                        for (int i = 0; i < cantSeleccion;i++)
-                        {
-                            global.EliminarGram();
-                        }
-
-                        Gramilla g = new Gramilla("",0);
-                        MessageBox.Show(g.mensajeAlCliente());
-                    }
-                    else
+                    MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                }
+                else
+                {
+                    int cantSeleccion = int.Parse(VerificarSelecccion().Text);
+                    Cliente cliente = global.buscarCliente(txtCedula.Text);
+                    if (cliente != null)
                     {
-                        MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                        cant = cliente.cantidadEntradas();
+                        if (cant < 3 && (cant + cantSeleccion) < 4)
+                        { 
+                            cliente.asignarEntrada(cantSeleccion,global.primerGR());
+                            for (int i = 0; i < cantSeleccion;i++)
+                            {
+                                global.EliminarGram();
+                            }
+
+                            Gramilla g = new Gramilla("",0);
+                            MessageBox.Show(g.mensajeAlCliente());
+                        }
+                        else
+                        {
+                            MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                        }
                     }
                 }
             }
@@ -98,32 +79,39 @@ namespace Practica_ClasesMDI
 
         private void btnGramilla_Click(object sender, EventArgs e)
         {
-            int cant = 0;
-            if (VerificarSelecccion() == null)
+            if (global.getGeneral().Count == 0)
             {
-                MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                MessageBox.Show("No quedan entradas!");
             }
             else
             {
-                int cantSeleccion = int.Parse(VerificarSelecccion().Text);
-                Cliente cliente = global.buscarCliente(txtCedula.Text);
-                if (cliente != null)
+                int cant = 0;
+                if (VerificarSelecccion() == null)
                 {
-                    cant = cliente.cantidadEntradas();
-                    if (cant < 3 && (cant + cantSeleccion) < 4)
+                    MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                }
+                else
+                {
+                    int cantSeleccion = int.Parse(VerificarSelecccion().Text);
+                    Cliente cliente = global.buscarCliente(txtCedula.Text);
+                    if (cliente != null)
                     {
-                        cliente.asignarEntrada(cantSeleccion, global.primerGen());
-                        for (int i = 0; i < cantSeleccion; i++)
+                        cant = cliente.cantidadEntradas();
+                        if (cant < 3 && (cant + cantSeleccion) < 4)
                         {
-                            global.EliminarGen();
-                        }
+                            cliente.asignarEntrada(cantSeleccion, global.primerGen());
+                            for (int i = 0; i < cantSeleccion; i++)
+                            {
+                                global.EliminarGen();
+                            }
 
-                        General gen = new General("", 0);
-                        MessageBox.Show(gen.mensajeAlCliente());
-                    }
-                    else
-                    {
-                        MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                            General gen = new General("", 0);
+                            MessageBox.Show(gen.mensajeAlCliente());
+                        }
+                        else
+                        {
+                            MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                        }
                     }
                 }
             }
@@ -131,31 +119,38 @@ namespace Practica_ClasesMDI
 
         private void btnVip_Click(object sender, EventArgs e)
         {
-            int cant = 0;
-            if (VerificarSelecccion() == null)
+            if (global.getPalcoVip().Count == 0)
             {
-                MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                MessageBox.Show("No quedan entradas!");
             }
             else
             {
-                int cantSeleccion = int.Parse(VerificarSelecccion().Text);
-                Cliente cliente = global.buscarCliente(txtCedula.Text);
-                if (cliente != null)
+                int cant = 0;
+                if (VerificarSelecccion() == null)
                 {
-                    cant = cliente.cantidadEntradas();
-                    if (cant < 3 && (cant + cantSeleccion) < 4)
+                    MessageBox.Show("Error, debe seleccionar la cantidad de entradas");
+                }
+                else
+                {
+                    int cantSeleccion = int.Parse(VerificarSelecccion().Text);
+                    Cliente cliente = global.buscarCliente(txtCedula.Text);
+                    if (cliente != null)
                     {
-                        cliente.asignarEntrada(cantSeleccion, global.primerVip());
-                        for (int i = 0; i < cantSeleccion; i++)
+                        cant = cliente.cantidadEntradas();
+                        if (cant < 3 && (cant + cantSeleccion) < 4)
                         {
-                            global.EliminarVip();
+                            cliente.asignarEntrada(cantSeleccion, global.primerVip());
+                            for (int i = 0; i < cantSeleccion; i++)
+                            {
+                                global.EliminarVip();
+                            }
+                            PalcoVIP vip = new PalcoVIP("", 0, 0);
+                            MessageBox.Show(vip.mensajeAlCliente());
                         }
-                        PalcoVIP vip = new PalcoVIP("", 0,0);
-                        MessageBox.Show(vip.mensajeAlCliente());
-                    }
-                    else
-                    {
-                        MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                        else
+                        {
+                            MessageBox.Show("El cliente no puede tener más de 3 entradas!");
+                        }
                     }
                 }
             }
